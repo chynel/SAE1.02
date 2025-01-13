5134ef3dc80300b651245c5d0dd6fdfa74879dd8
Chynel <chynelsoki50@gmail.com>
2025-01-13 11:15:31 +0100

    15

--- Lot1/mailingList.py
+++ Lot1/mailingList.py
@@ -1,5 +1,5 @@
 # date : 7 janvier 2025
-# auteurs : Ferrand SOKI, Salma ALAHYAN, Yasmina TARAZA
+# auteurs : Ferrand SOKI, Salma ALAHYAN, Yasmine TARAZA
 # version 8
 
 # Ce programme liste les fichiers contenus dans le dossier data 
